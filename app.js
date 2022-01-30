8ed836ac560bad68aeafa0d09fd28be0886651e8
Eoin Carrick <22carri99@gmail.com>
2022-01-30 16:18:23 +0000

    master

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const increase = document.querySelector(".increase");
 const decrease = document.querySelector(".decrease");
 const reset = document.querySelector(".reset");
 const div = document.createElement("div");
+const coding = document.createElement("code");
+coding.setAttribute("class", "codeBlocks");
+body.append(coding);
 div.setAttribute("class", "display");
 container.appendChild(div);
 const display = document.querySelector(".display");
@@ -16,7 +20,6 @@ allButtons.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const classesForIncrease = e.currentTarget.classList[1];
     const classesForDecrease = e.currentTarget.classList[1];
-    const classesForReset = e.currentTarget.classList[1];
     if (classesForIncrease === "increase") {
       count++;
     } else if (classesForDecrease === "decrease") {
